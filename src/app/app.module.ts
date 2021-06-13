8ee6a91c9bee6973e347d34d4a5f3f35e1b1c26c
Quynh <71189807+quynth@users.noreply.github.com>
2021-06-13 11:05:26 -0700

    Added sample data and wrote service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherService } from './weather.service';
 @NgModule({
-  declarations: [
-    AppComponent,
-    CurrentWeatherComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CurrentWeatherComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [WeatherService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,24 @@
+import { useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  styleUrls: ['./current-weather.component.css'],
 })
 export class CurrentWeatherComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  current: ICurrentWeather;
+  constructor() {
+    this.current = {
+      city: 'Redmond',
+      country: 'US',
+      date: new Date(),
+      image: '',
+      temperature: 59,
+      description: 'Rainy',
+    };
   }
 
+  ngOnInit(): void {}
 }
